25eaeb55b7fa0bb43b2585c19eb5a48b3712e111
SetupFailed <thomas.karlsson619@gmail.com>
2022-12-08 16:02:15 +0100

    first Commit

---
+++ interactiveStory_v2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Raylib_cs;
+
+int widthX = 1600;
+int heightY = 900;
+
+
+
+void choiceCircles(){
+  Raylib.DrawCircle(450,heightY-200,75,Color.WHITE);
+  Raylib.DrawCircle(widthX-450, heightY-200,75,Color.WHITE);
+}
+
+void character(){
+  Raylib.DrawCircle(widthX/2,heightY-200,30,Color.WHITE);
+}
+
+Raylib.InitWindow(widthX, heightY, "The title of my window");
+Raylib.SetTargetFPS(60);
+
+
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  
+  Raylib.ClearBackground(Color.BLACK);
+  
+  character();
+
+  choiceCircles();  
+  
+  Raylib.EndDrawing();
+}
